727f3a728d8f3eaccc9c09801d3b7429f275a050
Shavius <glamurparty@ukr.net>
2024-09-21 02:05:28 +0300

    fix code format

--- src/ts/modules/hellowInConsole.ts
+++ src/ts/modules/hellowInConsole.ts
@@ -1,7 +1,7 @@
-export function hellowInConsole(text: string = "No text"): string {
+export function hellowInConsole(text = "No text"): string {
 	return text;
 }
 
-export function plus(n1: number = 0, n2: number = 0): number {
+export function plus(n1 = 0, n2 = 0): number {
 	return n1 + n2;
 }
